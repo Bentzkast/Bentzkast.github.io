f6894e252f7279b37f28dab0d689aa152243803f
joseph-alfredo <jalfredopoly@gmail.com>
2020-12-25 13:42:08 -0800

    Simpler one

--- app/src/js/App.js
+++
@@ -1,8 +0,0 @@
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
-import StickyHeader from './modules/StickyHeader';
-import $ from 'jquery';
-
-var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll($('.reveal-on-scroll'), '85%');
-var stickyHeader = new StickyHeader();
